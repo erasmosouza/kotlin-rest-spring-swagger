6ea6a72be12324ebcd07adc855117adca4aa8e62
erasmo.pinheiro <erasmosouza@gmail.com>
2019-10-25 14:53:03 -0300

    Documentando um método com Swagger

--- src/main/kotlin/esp/quickstart/HealthController.kt
+++ src/main/kotlin/esp/quickstart/HealthController.kt
@@ -1,5 +1,6 @@
 package esp.quickstart
 
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -10,6 +11,7 @@ class HealthController {
 
     val counter = AtomicLong()
 
+    @ApiOperation(value = "Health Check to test the environment")
     @GetMapping("/check")
     fun check(@RequestParam("name", defaultValue = "World") name: String) =
         Health(counter.incrementAndGet(), "Hello, $name")
