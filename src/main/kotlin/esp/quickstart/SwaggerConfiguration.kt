29a7ea4930789fd838aa6c41bd50d80f995e5815
erasmo.pinheiro <erasmosouza@gmail.com>
2019-10-25 08:54:58 -0300

    Inclusão de informações adicionais sobre api no SwaggerConfiguration

--- src/main/kotlin/esp/quickstart/SwaggerConfiguration.kt
+++ src/main/kotlin/esp/quickstart/SwaggerConfiguration.kt
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.service.Contact
+import springfox.documentation.service.VendorExtension
+import java.util.ArrayList
+
+
 
 @Configuration
 @EnableSwagger2
@@ -18,4 +24,22 @@ class SwaggerConfiguration {
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
         .build()
+        .apiInfo(apiInfo())
+
+
+    fun apiInfo(): ApiInfo {
+
+        val vendorExtensions = ArrayList<VendorExtension<*>>()
+
+        return ApiInfo(
+            "Minha API Rest",
+            "Descricao da minha api",
+            "V0.3",
+            "www.erasmopinheiro.com",
+            Contact("Erasmo Pinheiro", "www.erasmopinheiro.com", "erasmo@erasmopinheiro.com"),
+            "Licence of API",
+            "www.erasmopinheiro.com/licence",
+            vendorExtensions
+        )
+    }
 }
